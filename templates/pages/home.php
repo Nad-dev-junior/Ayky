099693d95ccfd1ae2355296fd5cd38353e08aa79
Nadia <nadiaalmoustapha0@gmail.com>
2026-07-12 02:18:18 +0200

    ajout du footer plus media queries

--- templates/pages/home.php
+++ templates/pages/home.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Ayky</title>
   <link rel="stylesheet" href="/assets/styles/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 
 <body>
@@ -66,12 +67,62 @@
             <img src="assets/img/sp-patisserie2.jpg" alt="sp-patisserie2" class="fade-slide">
             <img src="assets/img/pizza.jpg" alt="pizza" class="fade-slide">
           </div>
-          
+
         </div>
 
       </div>
     </section>
+    <section class="entreprises">
+      <div class="entreprises-container">
+        <h2>Entreprises Partenaires</h2>
+        <div class="slide-entreprises">
+          <div class="entreprise-track">
+            <div class="entreprise-card">
+              <img src="assets/img/logo-entreprise1.png" alt="logo-entreprise1" class="entreprise-img " >
+            </div>
+            <div class="entreprise-card">
+               <img src="assets/img/logo-entreprise2.png" alt="logo-entreprise2" class="entreprise-img " >
+            </div>
+            <div class="entreprise-card">
+              <img src="assets/img/logo-entreprise3.png" alt="logo-entreprise3" class="entreprise-img " >
+            </div>
+            <div class="entreprise-card">
+              <img src="assets/img/logo-entreprise4.png" alt="logo-entreprise4" class="entreprise-img " >
+            </div>
+            <div class="entreprise-card">
+               <img src="assets/img/logo-entreprise5.png" alt="logo-entreprise5" class="entreprise-img " >
+            </div>
+            <div class="entreprise-card">
+               <img src="assets/img/logo-entreprise6.png" alt="logo-entreprise6" class="entreprise-img " >
+            </div>
+          </div>
+     
+        </div>
+      </div>
+    </section>
   </main>
+  <footer class="footer">
+    <div class="footer-container">
+      <p class="footer-logo">Ayky</p>
+      <p class="footer-text">Trouver votre métier de reve dans la restauration à Niamey</p>
+
+      <div class="footer-socials">
+        <a href="#" aria-label="Facebook" class="social-link">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="#" aria-label="Instagram" class="social-link">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a href="#" aria-label="Twitter / X" class="social-link">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a href="#" aria-label="whatsApp" class="social-link">
+          <i class="fab fa-whatsapp"></i>
+        </a>
+      </div>
+      <p class="footer-copy">© 2026 Ayky -Tous droits réservés</p>
+    </div>
+  </footer>
   <script src="/assets/js/script.js"></script>
 </body>
 
